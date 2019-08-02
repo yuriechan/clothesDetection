3b219791e134007ca111c3e1ced76851fb6e5091
Yurie <cutecupcakeandcute@gmail.com>
2019-08-02 14:15:22 +0900

    #1: add state and componentDidMount()

--- clothes-detection/src/App.js
+++ clothes-detection/src/App.js
@@ -3,6 +3,21 @@ import './App.css';
 import MagicDropzone from 'react-magic-dropzone';
 
 class App extends Component {
+
+  // add data to manage inside this component (can mutate unlike props in functional component)
+  state = {
+    model: null,
+    preview: "",
+    predictions: []
+  };
+
+  // when model is always true (only for test)
+  componentDidMount() {
+    this.setState({
+      model: true
+    });
+  }
+
   render() {
     return (
       <div className="App">
