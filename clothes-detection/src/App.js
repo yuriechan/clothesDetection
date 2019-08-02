6dce640e05f6f3c366082f8a2bc932c114735e5d
Yurie <cutecupcakeandcute@gmail.com>
2019-08-02 14:18:55 +0900

    #1: add onDrop() to retrieve link of accepted image

--- clothes-detection/src/App.js
+++ clothes-detection/src/App.js
@@ -18,6 +18,15 @@ class App extends Component {
     });
   }
 
+  // assign URI of image when accepted ?
+    // console.log the value of state to understand the function of onDrop()
+  onDrop = (accepted, rejected, links) => {
+    this.setState({ 
+      preview: accepted[0].preview || links[0] },
+      function () {console.log(this.state.preview)
+    })
+  }
+  
   render() {
     return (
       <div className="App">
