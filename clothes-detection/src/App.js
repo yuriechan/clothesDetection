3973090e1d2e0d90050e2e3653d1f07e02484d77
Yurie <cutecupcakeandcute@gmail.com>
2019-08-07 13:30:31 +0900

    #1: Remove unused function

--- clothes-detection/src/App.js
+++ clothes-detection/src/App.js
@@ -61,43 +61,6 @@ class App extends Component {
       function () {console.log(this.state.preview)
     })
   }
-  
-  cropToCanvas = (image, canvas, ctx) => {
-    const naturalWidth = image.naturalWidth;
-    const naturalHeight = image.naturalHeight;
-
-    canvas.width = image.width;
-    canvas.height = image.height;
-    // area of canvas that is in color white (cleared)
-    ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
-
-    // set the area of uploaded image 
-    if (naturalWidth > naturalHeight) {
-      ctx.drawImage(
-        image,
-        (naturalWidth - naturalHeight) / 2,
-        0,
-        naturalHeight,
-        naturalHeight,
-        0,
-        0,
-        ctx.canvas.width,
-        ctx.canvas.height
-      );
-    } else {
-      ctx.drawImage(
-        image,
-        0,
-        (naturalHeight - naturalWidth) / 2,
-        naturalWidth,
-        naturalWidth,
-        0,
-        0,
-        ctx.canvas.width,
-        ctx.canvas.height
-      );
-    }
-  };
 
   render() {
     return (
