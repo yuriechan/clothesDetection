0e1b8b237b560c0bc73fb85af092d0aaf2d6939a
Yurie <cutecupcakeandcute@gmail.com>
2019-08-26 16:26:42 +0900

    #6: remove Major's API key

--- clothes-detection/src/App.js
+++ clothes-detection/src/App.js
@@ -1,53 +1,52 @@
-import React, {Component} from 'react';
-import './App.css';
-import MagicDropzone from 'react-magic-dropzone';
+import React, { Component } from "react";
+import "./App.css";
+import MagicDropzone from "react-magic-dropzone";
 
 function getImageAttributes(imageUri) {
-  const yurieApiKey = 'AIzaSyAugHyBMfx_NSZcrc1H4QZ0e1mtIMgHPCg';
-  const majorApiKey = 'AIzaSyB_LRUShuGlrxDwNvR9FBsyyiMUUGXZTb0';
+  const yurieApiKey = "AIzaSyAugHyBMfx_NSZcrc1H4QZ0e1mtIMgHPCg";
   const url = `https://vision.googleapis.com/v1/images:annotate?alt=json&key=${yurieApiKey}`;
   const requestParams = {
-    "requests": [
+    requests: [
       {
-        "image": {
-          "content" : imageUri,
-          "source": {
-            "imageUri": ''
-          }
+        image: {
+          content: imageUri,
+          source: {
+            imageUri: "",
+          },
         },
-        "features": [
+        features: [
           {
-            "maxResults": 0,
-            "type": "LABEL_DETECTION"
-          }
-        ]
-      }
-    ]
-  }
+            maxResults: 0,
+            type: "LABEL_DETECTION",
+          },
+        ],
+      },
+    ],
+  };
   const options = {
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify(requestParams),
   };
   fetch(url, options)
-  .then(response => response.json())
-  .then(imageAttributes => {
-    // valid return value : {labelAnnotations: [{}, {}, {}]
-    // invalide return value : {error: {object}}
-    const labelAnnotations = imageAttributes.responses[0].labelAnnotations;
-    // if property key is error, labelAnnotations will return undefined, which will be converted to false in conditional.
-    if (labelAnnotations) {
-      const labels = makeClothesLabel(labelAnnotations);
-      const matchedLabels = findClothesMatch(labels);
-      populateClothesMatch(matchedLabels);
-      console.log(labels);
-    }
-  })
-  .catch(error => console.error(error))
+    .then(response => response.json())
+    .then(imageAttributes => {
+      // valid return value : {labelAnnotations: [{}, {}, {}]
+      // invalide return value : {error: {object}}
+      const labelAnnotations = imageAttributes.responses[0].labelAnnotations;
+      // if property key is error, labelAnnotations will return undefined, which will be converted to false in conditional.
+      if (labelAnnotations) {
+        const labels = makeClothesLabel(labelAnnotations);
+        const matchedLabels = findClothesMatch(labels);
+        populateClothesMatch(matchedLabels);
+        console.log(labels);
+      }
+    })
+    .catch(error => console.error(error));
 }
 
 function makeClothesLabel(labelAnnotations) {
   let labelArr = [];
-  labelAnnotations.forEach(function(labelAnnotation){
+  labelAnnotations.forEach(function(labelAnnotation) {
     let label = labelAnnotation.description.toLowerCase();
     labelArr.push(label);
   });
@@ -55,47 +54,50 @@ function makeClothesLabel(labelAnnotations) {
 }
 
 function findClothesMatch(labelArr) {
+  const db = getDB();
+  let matchedArr = [];
 
-const db = getDB();
-let matchedArr = [];
-
-for (let i = 0, n = db.length; i < n; i++) {
+  for (let i = 0, n = db.length; i < n; i++) {
     let attributeArr = Object.values(db[i]);
     let onceMatched = false;
     for (let j = 0, m = labelArr.length; j < m; j++) {
-        if (onceMatched) {break;}
-        for (let k = 0, o = attributeArr.length; k < o; k++) {
-            if (onceMatched) {break;}
-            if (attributeArr[k].constructor === Array) {
-                for (let l = 0, p = attributeArr[k].length; l < p; l++) {
-                    if (attributeArr[k][l] === labelArr[j]) {
-                        matchedArr.push(i);
-                        onceMatched = true;
-                    }
-                }
-            }
-            if (attributeArr[k] === labelArr[j]) {
-                matchedArr.push(i);
-                onceMatched = true;
-            }
+      if (onceMatched) {
+        break;
+      }
+      for (let k = 0, o = attributeArr.length; k < o; k++) {
+        if (onceMatched) {
+          break;
         }
+        if (attributeArr[k].constructor === Array) {
+          for (let l = 0, p = attributeArr[k].length; l < p; l++) {
+            if (attributeArr[k][l] === labelArr[j]) {
+              matchedArr.push(i);
+              onceMatched = true;
+            }
+          }
+        }
+        if (attributeArr[k] === labelArr[j]) {
+          matchedArr.push(i);
+          onceMatched = true;
+        }
+      }
     }
-}
-console.log(matchedArr);
-return matchedArr;
+  }
+  console.log(matchedArr);
+  return matchedArr;
 }
 
 function populateClothesMatch(matchedArr) {
   const db = getDB();
   let resultsContainer = document.getElementById("Results__container");
   let resultsWrapper = document.getElementById("Results__wrapper");
-  for (let i = 0, n = matchedArr.length; i < n; i++){
+  for (let i = 0, n = matchedArr.length; i < n; i++) {
     let resultsChild = document.createElement("DIV");
-    resultsChild.id = (`Results__child__${i}`);
-    resultsChild.className = (`Results__child`);
+    resultsChild.id = `Results__child__${i}`;
+    resultsChild.className = `Results__child`;
     let img = document.createElement("IMG");
-    img.id = (`Results__child--img-${i}`);
-    img.className = ('Results__child--image');
+    img.id = `Results__child--img-${i}`;
+    img.className = "Results__child--image";
     img.src = db[matchedArr[i]].dataUrl || db[matchedArr[i]].dataUri;
     resultsChild.append(img);
     resultsWrapper.append(resultsChild);
@@ -107,117 +109,115 @@ function getDB() {
     {
       gender: "m", // male
       category: "shirt", // shirt
-      subCategory: ['tshirt', 'crew neck'], // ['tshirt', 'crew neck']
+      subCategory: ["tshirt", "crew neck"], // ['tshirt', 'crew neck']
       color: ["white"], // ['red', 'white']
       dataUri: "", // base64 encoded image
-      dataUrl: "/images/white-tshirt.jpg" // url of image
-    },
-    {
-      gender: "f", 
-      category: "skirt", 
-      subCategory: ['tennis skirt', 'Pleated skirt'], 
-      color: ["white", "vanilla"], 
-      dataUri: "", 
-      dataUrl: "/images/white_skirt_1.jpg"
-    },
-    {
-      gender: "f", 
-      category: "skirt", 
-      subCategory: ['tennis skirt', 'white camo'], 
-      color: ["white"], 
-      dataUri: "", 
-      dataUrl: "/images/white_skirt_2.jpg" 
+      dataUrl: "/images/white-tshirt.jpg", // url of image
     },
     {
       gender: "f",
-      category: "skirt", 
-      subCategory: ['tennis skirt', 'Sports wear'], 
-      color: ["white"], 
-      dataUri: "", 
-      dataUrl: "/images/white_skirt_3.jpg"
-    }
+      category: "skirt",
+      subCategory: ["tennis skirt", "Pleated skirt"],
+      color: ["white", "vanilla"],
+      dataUri: "",
+      dataUrl: "/images/white_skirt_1.jpg",
+    },
+    {
+      gender: "f",
+      category: "skirt",
+      subCategory: ["tennis skirt", "white camo"],
+      color: ["white"],
+      dataUri: "",
+      dataUrl: "/images/white_skirt_2.jpg",
+    },
+    {
+      gender: "f",
+      category: "skirt",
+      subCategory: ["tennis skirt", "Sports wear"],
+      color: ["white"],
+      dataUri: "",
+      dataUrl: "/images/white_skirt_3.jpg",
+    },
   ];
   return db;
 }
 
 class App extends Component {
-
   // add data to manage inside this component (can mutate unlike props in functional component)
   state = {
     model: null,
     preview: "",
-    predictions: []
+    predictions: [],
   };
 
   // when model is always true (only for test)
   componentDidMount() {
     this.setState({
-      model: true
+      model: true,
     });
   }
 
   // assign URI of image when accepted ?
-    // console.log the value of state to understand the function of onDrop()
+  // console.log the value of state to understand the function of onDrop()
   onDrop = (accepted, rejected, links) => {
-    this.setState({ 
-      preview: accepted[0].preview || links[0] },
-      function () {console.log(this.state.preview)
-    })
-  }
+    this.setState(
+      {
+        preview: accepted[0].preview || links[0],
+      },
+      function() {
+        console.log(this.state.preview);
+      },
+    );
+  };
 
   // convert local file => blob => base 64 (when uploaded image is local)
-  convertBase64 = (localFile) => {
-  fetch(localFile)
-  .then(function(response) {
-    return response.blob()
-  })
-  .then(function(blob) {
-    let reader = new FileReader(); 
-    reader.readAsDataURL(blob);
-    reader.onload = function () {
-      let rawBase64data = reader.result;
-      // remove the header string of base 64 data
-      let finalBase64data = rawBase64data.substr(rawBase64data.indexOf(',') + 1);
-      getImageAttributes(finalBase64data);
-    }
-  })
-  .catch(error => console.log(error))
-  }
+  convertBase64 = localFile => {
+    fetch(localFile)
+      .then(function(response) {
+        return response.blob();
+      })
+      .then(function(blob) {
+        let reader = new FileReader();
+        reader.readAsDataURL(blob);
+        reader.onload = function() {
+          let rawBase64data = reader.result;
+          // remove the header string of base 64 data
+          let finalBase64data = rawBase64data.substr(rawBase64data.indexOf(",") + 1);
+          getImageAttributes(finalBase64data);
+        };
+      })
+      .catch(error => console.log(error));
+  };
 
   render() {
     return (
-    <div>
-      <div className="Dropzone-page">
-        {this.state.model ? (
-          <MagicDropzone
-            className="Dropzone"
-            accept="image/jpeg, image/png, .jpg, .jpeg, .png"
-            multiple={false}
-            onDrop={this.onDrop}
-            onDragEnd={this.convertBase64(this.state.preview)}
-          >
-            <div className="Dropzone-content">
-              {this.state.preview ? (
-                <img 
-                  alt="upload preview"
-                  className="Dropzone-img"
-                  src={this.state.preview}
-                />
-              ) : (
-                "Choose or drop a file"
-              )}
-              <canvas id="canvas" />
-            </div>
-          </MagicDropzone> 
-        ) : (
-          <div className="Dropzone">Loading model</div>
-        )}
-      </div>
-      <div id="Results__container">
-        <div id="Results__wrapper">
+      <div>
+        <div className="Dropzone-page">
+          {this.state.model ? (
+            <MagicDropzone
+              className="Dropzone"
+              accept="image/jpeg, image/png, .jpg, .jpeg, .png"
+              multiple={false}
+              onDrop={this.onDrop}
+              onDragEnd={this.convertBase64(this.state.preview)}
+            >
+              <div className="Dropzone-content">
+                {this.state.preview ? (
+                  <img alt="upload preview" className="Dropzone-img" src={this.state.preview} />
+                ) : (
+                  "Choose or drop a file"
+                )}
+                <canvas id="canvas" />
+              </div>
+            </MagicDropzone>
+          ) : (
+            <div className="Dropzone">Loading model</div>
+          )}
+        </div>
+        <div id="Results__container">
+          <div id="Results__wrapper"></div>
         </div>
       </div>
-  </div>
     );
   }
 }
